024f6f61f814b20e12411b2634d6f054fd03d877
zaid-alrafayia <zaidomar678@gmail.com>
2026-04-26 23:02:35 +0300

    data interface 95% complete

--- structs.h
+++ structs.h
@@ -3,13 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   structs.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zaalrafa </var/spool/mail/zaalrafa>        +#+  +:+       +#+        */
+/*   By: zaalrafa                                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/24 01:29:23 by zaalrafa          #+#    #+#             */
 /*   Updated: 2026/04/24 01:47:23 by zaalrafa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdbool.h>
+#include <unistd.h>
+
+typedef enum e_token_type
+{
+	REDIRECT_IN,
+	REDIRECT_OUT,
+	CMD,
+	TRUNCATE,
+	APPEND,
+	HEREDOC,
+	PIPE,
+}					t_token_type;
+
+typedef struct s_token
+{
+	char			*value;
+	t_token_type	type;
+	int				quote_type;
+	struct s_token	*next;
+}					t_token;
 
 typedef struct s_env
 {
@@ -20,19 +40,23 @@ typedef struct s_env
 
 typedef struct s_cmd
 {
-	char			*main_cmd;
-	char			**flags;
+	char			**args;
 	int				infile;
 	int				outfile;
 	bool			append;
 	char			*limiter;
 	struct s_cmd	*next;
 }					t_cmd;
+
 typedef struct s_shell
 {
 	char			**history;
+	pid_t			*pids;
+	int				stdin_backup;
+	int				stdout_backup;
 	char			**env_array;
 	struct s_env	*env;
 	struct s_cmd	*current_cmd;
+	struct s_token	*tokens;
 	int				exit_status;
 }					t_shell;
